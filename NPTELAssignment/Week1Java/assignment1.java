e79f0e93aa3bc190442547db6c2432ae92f2094e
RayaanRacer <gopaltiwariorange@gmail.com>
2023-02-07 01:24:51 +0530

    assignment1

---
+++ NPTELAssignment/Week1Java/assignment1.java
@@ -0,0 +1,10 @@
+public class assignment1{
+    public static void main(String[] args){
+        /******* boolean b =false; *******/ //n1
+        String b= "false";
+        switch(b){ //n2
+            case "false":
+            System.out.println("a");
+        }
+    }
+}
