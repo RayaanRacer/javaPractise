77faa3ba4c0cc48ef51728b7110de3e3581827e1
RayaanRacer <gopaltiwariorange@gmail.com>
2023-02-07 01:48:25 +0530

    how?

---
+++ ass2.java
@@ -0,0 +1,6 @@
+public class ass2 {
+    public static void main(String[] args){
+        char nptel[]={'1','2','3','4','5','6'};
+        System.out.println(nptel[nptel.length-2]+nptel[0]);
+    }
+}
