a3b7e4698009e2b9ebd351ff0c3ca460e5897a8b
Gopal Tiwari <123818776+RayaanRacer@users.noreply.github.com>
2023-11-28 02:02:15 +0530

    Create BinaryTree creation.java

---
+++ BinaryTree creation.java
@@ -0,0 +1,31 @@
+class BinaryTreeM{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        static Node treeBuilder(int[] nodes){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = treeBuilder(nodes);
+            newNode.right = treeBuilder(nodes);
+            return newNode;
+        }
+    }
+    public static void main(String[] args){
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        System.out.print(tree.treeBuilder(nodes).data);
+
+    }
+}
